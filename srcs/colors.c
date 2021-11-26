7bd59c33fb5697720525414e7609fe3beec9a187
Nastasia Demont <nastasia@demont.me>
2021-11-26 20:56:54 +0100

    Colors okay

--- srcs/colors.c
+++ srcs/colors.c
@@ -15,7 +15,6 @@ void		color_pixel(t_wireframe *w,  t_point p1, t_point pt, t_point p2, float rat
 	limit_min = 0;
 	limit_max = 0.2;
 	pt.z = p1.z + (ratio * (p2.z - p1.z));
-	printf("z = %f	| ", pt.z);
 	diff_to_zero = 0 - w->min.z;
 	diff_total = w->max.z - w->min.z;
 	pt.z += diff_to_zero;
@@ -31,14 +30,18 @@ void		color_pixel(t_wireframe *w,  t_point p1, t_point pt, t_point p2, float rat
 	if (quarter < 0.2)
 	{
 		quarter = quarter / 0.2;
-		*(unsigned int *)str = 0x0000FF + (0xFF0000 * (1 - quarter));
+		w->window.data_addr[w->pixel.p + 2] = (char)(255 * (1 - quarter));
+	 	w->window.data_addr[w->pixel.p + 1] = 0;
+	 	w->window.data_addr[w->pixel.p + 0] = (char)255;
 		printf("color = %x\n", *str);
 
 	}
 	else if (quarter < 0.4)
 	{
 		quarter = (quarter - 0.2) / 0.2;
-		*(unsigned int *)str = 0x0000FF - (0x00FF00 * (quarter));
+		w->window.data_addr[w->pixel.p + 2] = 0;
+	 	w->window.data_addr[w->pixel.p + 1] = (char)(255 * quarter);
+	 	w->window.data_addr[w->pixel.p + 0] = (char)255;
 		printf("color = %x\n", *str);
 
 
@@ -46,22 +49,26 @@ void		color_pixel(t_wireframe *w,  t_point p1, t_point pt, t_point p2, float rat
 	else if (quarter < 0.6)
 	{
 		quarter = (quarter - 0.4) / 0.2;
-		*(unsigned int *)str = 0x00FF00 + (0x0000FF * (1 -quarter));
+		w->window.data_addr[w->pixel.p + 2] = 0;
+	 	w->window.data_addr[w->pixel.p + 1] = (char)255;
+	 	w->window.data_addr[w->pixel.p + 0] = (char)(255 * (1 - quarter));
 		printf("color = %x\n", *str);
-
-
 	}
 	else if (quarter < 0.8)
 	{
 		quarter = (quarter - 0.6) / 0.2;
-		*(unsigned int *)str = 0x00FF00 - (0xFF0000 * (quarter));
+		w->window.data_addr[w->pixel.p + 2] = (char)(255 * quarter);
+	 	w->window.data_addr[w->pixel.p + 1] = (char)255;
+	 	w->window.data_addr[w->pixel.p + 0] = 0;
 		printf("color = %x\n", *str);
 
 	}
 	else
 	{
 		quarter = (quarter - 0.8) / 0.2;
-		*(unsigned int *)str = 0xFF0000 + (0x00FF00 * (1 - quarter));
+		w->window.data_addr[w->pixel.p + 2] = (char)255;
+	 	w->window.data_addr[w->pixel.p + 1] = (char)(255 * (1 - quarter));
+	 	w->window.data_addr[w->pixel.p + 0] = 0;
 		printf("color = %x\n", *str);
 
 	}
